223517beb3c156789b715f678bb1ed646a9ea790
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-04-14 22:02:20 +0200

    2: Initial DB migration

---
+++ Agora.Infrastructure/Data/AgoraDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Agora.Core.Models;
+using Agora.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agora.Infrastructure.Data;
+
+public class AgoraDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<PostCategory> PostCategories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<TransactionStatus> TransactionStatus { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new PostCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        modelBuilder.ApplyConfiguration(new TransactionStatusConfiguration());
+    }
+    
+};
+
+
+
