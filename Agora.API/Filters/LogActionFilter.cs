4982d312bc72cb09fb440d984c68315b70df2a54
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-05-05 14:16:48 +0200

    9 - Implement Serilog and add more details when calling endpoints of controllers

---
+++ Agora.API/Filters/LogActionFilter.cs
@@ -0,0 +1,73 @@
+﻿using Agora.Core.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace Agora.API.Filters;
+
+// Filter (like a hook) to add logs related to actions methods in controllers
+public class LogActionFilter(ILogger<LogActionFilter> logger) : IActionFilter
+{
+    // Before the action methods runs
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        logger.LogInformation("\u27a1  Entering action {ActionName} with parameters {Parameters}", 
+            context.ActionDescriptor.DisplayName,
+            context.ActionArguments.Values);
+        
+        if (!context.ModelState.IsValid)
+        {
+            Dictionary<string, List<string>> validationErrors = ModelStateExtensions.ExtractErrors(context.ModelState);
+            logger.LogWarning("\u26A0  Data validation has failed for action {ActionName}. Errors: {@ValidationErrors}",
+                context.ActionDescriptor.DisplayName,
+                validationErrors);
+
+            // Retourner un BadRequest personnalisé avec les détails d'erreur dans la réponse
+            context.Result = new BadRequestObjectResult(context.ModelState);
+        }
+    }
+
+    // After the action methods runs
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        // Extract return result of the action method
+        switch (context.Result)
+        {
+            
+            case BadRequestObjectResult badRequest:
+            {
+                var details = badRequest.Value;
+
+                if (details is ValidationProblemDetails validationProblemDetails)
+                {
+                    Log.Warning("\u2b05  Exiting action {ActionName}. Result is BadRequest with errors: {@Errors}",
+                        context.ActionDescriptor.DisplayName,
+                        validationProblemDetails.Errors);
+                }
+                else
+                {
+                    Log.Warning("\u2b05  Exiting action {ActionName}. Result is BadRequest with details  {@Details}",
+                        context.ActionDescriptor.DisplayName,
+                        details);
+                }
+
+                break;
+            }
+            case ObjectResult result:
+                logger.LogInformation("\u2b05  Exiting action {ActionName}: {StatusCode} - {Result}",
+                    context.ActionDescriptor.DisplayName,
+                    result.StatusCode,
+                    result.Value);
+                break;
+            case StatusCodeResult statusResult:
+                Log.Information("\u2b05  Exiting action {ActionName}: {StatusCode}",
+                    context.ActionDescriptor.DisplayName,
+                    statusResult.StatusCode);
+                break;
+            default:
+                Log.Information("\u2b05  Exiting action {ActionName} with unknown result", 
+                    context.ActionDescriptor.DisplayName);
+                break;
+        }
+    }
+}
---
+++ Agora.Core/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Agora.Core.Extensions;
+
+public static class ModelStateExtensions
+{
+    // Extract the errors for all fields not compliant with data annotations (in DTO or model)
+    public static Dictionary<string, List<string>> ExtractErrors(ModelStateDictionary modelState)
+    {
+        return modelState
+            .Where(ms => ms.Value != null && ms.Value.Errors.Any())
+            .ToDictionary(
+                ms => ms.Key,
+                ms => ms.Value!.Errors.Select(e => e.ErrorMessage).ToList()
+            );
+    }
+}
