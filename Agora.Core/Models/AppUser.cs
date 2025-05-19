20119088633bf84f37f335fef1e802d50349bed4
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-05-19 23:13:49 +0200

    11 - Add navigation properties and date properties to AppUser

--- Agora.Core/Models/AppUser.cs
+++ Agora.Core/Models/AppUser.cs
@@ -5,4 +5,10 @@ namespace Agora.Core.Models;
 public class AppUser : IdentityUser
 {
     public int Credit { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastLoginAt { get; set; }
+    
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    public ICollection<Transaction> TransactionsAsBuyer { get; set; } = new List<Transaction>();
+    public ICollection<Transaction> TransactionsAsSeller { get; set; } = new List<Transaction>();
 }
