64beab7242af6fe75e65a98574fde7ac3a30ff9d
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-05-20 15:36:32 +0200

    11 - Remove user model and related classes without Identity

--- Agora.API/DTOs/User/UserSummaryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Agora.API.DTOs.User;
-
-public class UserSummaryDto
-{
-    public long Id { get; set; }
-    public string Username { get; set; } = String.Empty;
-    public string Email { get; set; } = String.Empty;
-    public int Credit { get; set; }
-}
