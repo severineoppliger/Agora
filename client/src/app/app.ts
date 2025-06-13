8a5382bd0230b04535bf98d96396760146a7478c
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-06-13 11:10:01 +0200

    14 - Header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header'
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+
+  baseApiUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   protected title = 'Agora';
+  posts: any[] = []
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseApiUrl + 'posts').subscribe({
+      next: response => this.posts = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatIcon,
+    MatButton
+  ],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss']
+})
+export class Header {
+
+}
