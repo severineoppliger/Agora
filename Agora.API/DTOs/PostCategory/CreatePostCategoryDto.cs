9508ad99ed707d9e06c9bfaffcde40674ec437db
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-06-15 23:21:13 +0200

    15 - Separate input post category DTOs into one for creation and one for update

--- Agora.API/DTOs/PostCategory/CreatePostCategoryDto.cs
+++ Agora.API/DTOs/PostCategory/CreatePostCategoryDto.cs
@@ -1,5 +1,13 @@
-﻿namespace Agora.API.DTOs.PostCategory;
+﻿using System.ComponentModel.DataAnnotations;
+using Agora.Core.Validation;
 
-public class CreatePostCategoryDto: BaseInputPostCategoryDto
+namespace Agora.API.DTOs.PostCategory;
+
+public class CreatePostCategoryDto
 {
+    [Required]
+    [NotEmptyOrWhitespace]
+    [MinLength(ValidationRules.PostCategory.NameMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
+    [MaxLength(ValidationRules.PostCategory.NameMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
+    public string Name { get; set; } = String.Empty;
 }
--- Agora.API/DTOs/PostCategory/UpdatePostCategoryDto.cs
+++ Agora.API/DTOs/PostCategory/UpdatePostCategoryDto.cs
@@ -1,5 +1,13 @@
-﻿namespace Agora.API.DTOs.PostCategory;
+﻿using System.ComponentModel.DataAnnotations;
+using Agora.Core.Validation;
 
-public class UpdatePostCategoryDto : BaseInputPostCategoryDto
+namespace Agora.API.DTOs.PostCategory;
+
+public class UpdatePostCategoryDto
 {
+    [Required]
+    [NotEmptyOrWhitespace]
+    [MinLength(ValidationRules.PostCategory.NameMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
+    [MaxLength(ValidationRules.PostCategory.NameMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
+    public string NewName { get; set; } = String.Empty;
 }
