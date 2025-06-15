9bb038b398e94b8da0cd302f5487f44258d68526
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-06-15 23:22:43 +0200

    15 - Separate input post DTOs into one for creation and one for update

--- Agora.API/DTOs/Post/CreatePostDto.cs
+++ Agora.API/DTOs/Post/CreatePostDto.cs
@@ -1,5 +1,29 @@
-﻿namespace Agora.API.DTOs.Post;
+﻿using System.ComponentModel.DataAnnotations;
+using Agora.Core.Validation;
 
-public class CreatePostDto : BaseInputPostDto
+namespace Agora.API.DTOs.Post;
+
+public class CreatePostDto
 {
+    [Required]
+    [NotEmptyOrWhitespace]
+    [MinLength(ValidationRules.Post.TitleMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
+    [MaxLength(ValidationRules.Post.TitleMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
+    public string Title { get; set; } = String.Empty;
+    
+    [Required]
+    [NotEmptyOrWhitespace]
+    [MinLength(ValidationRules.Post.DescriptionMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
+    [MaxLength(ValidationRules.Post.DescriptionMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
+    public string Description { get; set; } = String.Empty;
+    
+    [Required]
+    [Range(ValidationRules.Post.PriceMin, ValidationRules.Post.PriceMax, ErrorMessage = "{0} must be between {1} and {2}.")]
+    public int Price { get; set; }
+    
+    [Required]
+    public string Type { get; set; } = String.Empty;
+    
+    [Required]
+    public long PostCategoryId { get; set; }
 }
--- Agora.API/DTOs/Post/UpdatePostDetailsDto.cs
+++ Agora.API/DTOs/Post/UpdatePostDetailsDto.cs
@@ -3,27 +3,20 @@ using Agora.Core.Validation;
 
 namespace Agora.API.DTOs.Post;
 
-public abstract class BaseInputPostDto
+public class UpdatePostDetailsDto
 {
-    [Required]
-    [NotEmptyOrWhitespace]
     [MinLength(ValidationRules.Post.TitleMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
     [MaxLength(ValidationRules.Post.TitleMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
-    public string Title { get; set; } = String.Empty;
+    public string? Title { get; set; } = String.Empty;
     
-    [Required]
-    [NotEmptyOrWhitespace]
     [MinLength(ValidationRules.Post.DescriptionMinLength, ErrorMessage = "{0} must be at least {1} characters.")]
     [MaxLength(ValidationRules.Post.DescriptionMaxLength, ErrorMessage = "{0} must be less than {1} characters.")]
-    public string Description { get; set; } = String.Empty;
+    public string? Description { get; set; } = String.Empty;
     
-    [Required]
     [Range(ValidationRules.Post.PriceMin, ValidationRules.Post.PriceMax, ErrorMessage = "{0} must be between {1} and {2}.")]
-    public int Price { get; set; }
+    public int? Price { get; set; }
     
-    [Required]
-    public string Type { get; set; } = String.Empty;
+    public string? Type { get; set; } = String.Empty;
     
-    [Required]
-    public long PostCategoryId { get; set; }
+    public long? PostCategoryId { get; set; }
 }
