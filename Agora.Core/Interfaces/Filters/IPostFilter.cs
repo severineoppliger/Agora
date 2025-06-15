8f27eee36fd3bf8c8a1afd76ba473f1af6f0eae5
Séverine Oppliger <severine.oppliger@unifr.ch>
2025-06-16 00:15:42 +0200

    15 - Improve post filters

--- Agora.Core/Interfaces/Filters/IPostFilter.cs
+++ Agora.Core/Interfaces/Filters/IPostFilter.cs
@@ -6,7 +6,6 @@ public interface IPostFilter
     public int? MinPrice { get; set; }
     public int? MaxPrice { get; set; }
     public string? TypeName { get; set; }
-    public string? StatusName { get; set; }
     public string? PostCategoryName { get; set; }
     public string? UserName { get; set; }
     public string? SortBy { get; set; }
